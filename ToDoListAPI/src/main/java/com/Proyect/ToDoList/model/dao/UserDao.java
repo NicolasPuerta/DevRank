d2cf3ae37f9ce4245e150ac86fc2f83e911176c8
GDARKKING <jdiego@Portatil-50-2.tsisl.dom>
2024-05-18 15:01:17 +0200

    delet api SpringBoot

--- ToDoListAPI/src/main/java/com/Proyect/ToDoList/model/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.Proyect.ToDoList.model.dao;
-
-import com.Proyect.ToDoList.model.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDao extends CrudRepository<User, Long> {
-}
